dcee851e4f3322301b8481f054beb733224333ba
AikaNiyazova <aikaniyazova@gmail.com>
2022-12-04 03:46:26 +0600

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String num = String.valueOf(number);
+        char[] digits = num.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < digits.length; i++) {
+            sum += num.charAt(i);
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,17 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class NumberReverter {
     public void revert(int number) {
+        String num = String.valueOf(number);
+        List<char[]> digits = new ArrayList<>(Collections.singleton(num.toCharArray()));
+        List<char[]> reversed = new ArrayList<>();
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            reversed.add(digits.get(i));
+        }
+        System.out.println(reversed);
     }
 }
