fdfcab7657431cf5e351002ee659168e35da4ca4
AikaNiyazova <aikaniyazova@gmail.com>
2022-12-04 18:58:59 +0600

    2 attepmt

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,10 +3,10 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         String num = String.valueOf(number);
-        char[] digits = num.toCharArray();
+//        char[] digits = num.toCharArray();
         int sum = 0;
-        for (int i = 0; i < digits.length; i++) {
-            sum += num.charAt(i);
+        for (int i = 0; i < num.length(); i++) {
+            sum += Character.getNumericValue(num.charAt(i));
         }
         System.out.println(sum);
     }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -7,10 +7,11 @@ import java.util.List;
 public class NumberReverter {
     public void revert(int number) {
         String num = String.valueOf(number);
-        List<char[]> digits = new ArrayList<>(Collections.singleton(num.toCharArray()));
-        List<char[]> reversed = new ArrayList<>();
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            reversed.add(digits.get(i));
+//        List<char[]> digits = new ArrayList<>(Collections.singleton(num.toCharArray()));
+//        List<char[]> reversed = new ArrayList<>();
+        String reversed = new String();
+        for (int i = num.length() - 1; /*digits.size() - 1;*/ i >= 0; i--) {
+            reversed.concat(String.valueOf(Character.getNumericValue(num.charAt(i))))/*.add(digits.get(i))*/;
         }
         System.out.println(reversed);
     }
